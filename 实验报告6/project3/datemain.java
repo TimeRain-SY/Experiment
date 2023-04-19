026540f2f74b0fe53dfe14bd26e781be31959e55
TimeRain-SY <2582275515@qq.com>
2023-04-19 13:28:07 +0800

    2023/4/19

--- 实验报告6/project3/datemain.java
+++ 实验报告6/project3/datemain.java
@@ -4,7 +4,11 @@ import java.util.Scanner;
 
 public class datemain {
     public static void main(String[] args){
-        date date1 = new date(2022L,13, 14f);
-
+        date date1 = new date(2022L,11, 14f);
+        date date2 = new date(24f,1, 2023L);
+        date date3 = new date(4,5f,2021L);
+        date1.test();
+        date2.test();
+        date3.test();
     }
 }
