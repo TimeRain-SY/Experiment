53cf15873df634efc33ac678999fd39d346bb6b9
TimeRain-SY <2582275515@qq.com>
2023-04-19 14:05:54 +0800

    2023/4/19

--- 实验报告6/project3/datemain.java
+++ 实验报告6/project3/datemain.java
@@ -7,8 +7,5 @@ public class datemain {
         date date1 = new date(2022L,11, 14f);
         date date2 = new date(24f,1, 2023L);
         date date3 = new date(4,5f,2021L);
-        date1.test();
-        date2.test();
-        date3.test();
     }
 }
